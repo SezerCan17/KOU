6ad88ddb8dd4333b695f742def5da2278d523531
OmerYurtalan <116679408+OmerYurtalan@users.noreply.github.com>
2023-12-08 22:57:53 +0300

    ......

--- Assets/Bullet.cs
+++ Assets/Bullet.cs
@@ -4,16 +4,16 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-    movment mov;
-    public float speed = 20f;
-    public Rigidbody2D rb;
-    void Start()
-    {
-		  rb.velocity = transform.right * speed;
+
+	
+	public float speed = 20f;
+	public Rigidbody2D rb;
+	void Start()
+	{
+		rb.velocity = transform.right * speed;
 	}
 	void Update()
 	{
-
-		Destroy(gameObject,1f);
+		Destroy(gameObject, 1f);
 	}
 }
